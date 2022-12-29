5dc61d33af94d583adc7021bc6b6c89f3bc0a9f2
Purna Shrestha <sthapurnait@kbc.edu.np>
2022-12-29 23:29:54 +0545

    added popup

---
+++ assets/js/script.js
@@ -0,0 +1,22 @@
+//For button animation show
+$(".button").click(function () {
+  const modal = $(this).parent().parent().parent().find(".modal-container");
+  modal.removeAttr("class").addClass("modal-container show-content");
+  $("body").addClass("modal-active");
+});
+
+$(".modal-container").click(function () {
+  $(this).addClass("out");
+  $("body").removeClass("modal-active");
+});
+
+// for dropdown content of the questions.
+$(document).ready(function () {
+  $("#sidebarCollapse").on("click", function () {
+    $("#sidebar").toggleClass("active");
+  });
+});
+var myLink = document.querySelector('a[href="#"]');
+myLink.addEventListener("click", function (e) {
+  e.preventDefault();
+});
